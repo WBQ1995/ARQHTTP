1f4148e87db046956cbfea0d432d47b423b0bcaf
WBQ1995 <boqianwang1995@gmail.com>
2018-11-28 19:18:53 -0500

    server new connection thread killed if the connection is not established

--- src/main/java/Server/NewConnection.java
+++ src/main/java/Server/NewConnection.java
@@ -2,15 +2,16 @@ package Server;
 
 import Packet.Packet;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
 import java.nio.channels.DatagramChannel;
+import java.util.HashMap;
 import java.util.TreeMap;
-
-import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class NewConnection implements Runnable {
 
@@ -23,15 +24,13 @@ public class NewConnection implements Runnable {
 
     private boolean connected = false;
 
-    private String rcvData = "";
-
-    private boolean fin = false;
+    private boolean allSent = false;
 
     private long startNumber = -1;
     private long endNumber = -1;
-    private TreeMap<Long,String> window;
+    private TreeMap<Long,String> rcvWindow;
 
-    public boolean allRecieved = false;
+    private HashMap<Long,Boolean> sendWindow;
 
     public NewConnection(Packet packet){
         this.packet = packet;
@@ -40,7 +39,8 @@ public class NewConnection implements Runnable {
         ackNumber = packet.getSequenceNumber();
         sequenceNumber = (long) (Math.random() * 1000);
 
-        window = new TreeMap<>();
+        rcvWindow = new TreeMap<>();
+        sendWindow = new HashMap<>();
 
         try{
             channel = DatagramChannel.open();
@@ -60,16 +60,124 @@ public class NewConnection implements Runnable {
 
             Thread.sleep(2000);
 
-            for (long key:window.keySet()){
-                data += window.get(key);
-            }
+            if(!connected)
+                return;
 
+            for (long key: rcvWindow.keySet()){
+                data += rcvWindow.get(key);
+            }
+            System.out.println("received request!");
             System.out.println(data);
 
+            File myFile = new File("/Users/WBQ/IdeaProjects/ARQHTTP/src/main/java/Server/text.txt");
+            FileReader fileReader = new FileReader(myFile);
+            BufferedReader reader = new BufferedReader(fileReader);
+            String response = "";
+            String line = null;
+            while ((line = reader.readLine()) != null){
+                response += line;
+            }
+            reader.close();
+
+            sendResponse(response);
+
+            receiveAck();
+
+            System.out.println("connection done!");
         } catch (IOException ex){
             ex.getStackTrace();
         } catch (InterruptedException ex){
             ex.printStackTrace();
+        }
+    }
+
+    private void receiveAck() throws IOException{
+        ByteBuffer buf = ByteBuffer.allocate(Packet.MAX_LEN);
+        while (true){
+            buf.clear();
+            channel.receive(buf);
+            buf.flip();
+            Packet resp = Packet.fromBuffer(buf);
+            if(resp.getType() == 4){
+                allSent = true;
+                sendWindow.put(resp.getSequenceNumber(),true);
+                if(responseAllSent())
+                    break;
+            }
+        }
+    }
+
+    private void sendResponse(String response){
+        SendPacket sendPacket;
+        Thread sendPacketThread;
+
+        if(response.length() < 1000){
+            Packet dataPacket = new Packet.Builder()
+                    .setType(0)
+                    .setSequenceNumber(sequenceNumber)
+                    .setPortNumber(packet.getPeerPort())
+                    .setPeerAddress(packet.getPeerAddress())
+                    .setPayload(response.getBytes())
+                    .create();
+
+            sendWindow.put(sequenceNumber,false);
+
+            sendPacket = new SendPacket(dataPacket,this);
+            sendPacketThread = new Thread(sendPacket);
+            sendPacketThread.start();
+
+            return;
+        } else {
+            String firstData = response.substring(0,1000);
+            response = response.substring(1000);
+            Packet firstPacket = new Packet.Builder()
+                    .setType(5)
+                    .setSequenceNumber(sequenceNumber)
+                    .setPortNumber(packet.getPeerPort())
+                    .setPeerAddress(packet.getPeerAddress())
+                    .setPayload(firstData.getBytes())
+                    .create();
+
+            sendWindow.put(sequenceNumber,false);
+
+            sendPacket = new SendPacket(firstPacket,this);
+            sendPacketThread = new Thread(sendPacket);
+            sendPacketThread.start();
+            sequenceNumber++;
+
+            while (response.length() > 1000){
+                String middleData = response.substring(0,1000);
+                response = response.substring(1000);
+                Packet middlePacket = new Packet.Builder()
+                        .setType(6)
+                        .setSequenceNumber(sequenceNumber)
+                        .setPortNumber(packet.getPeerPort())
+                        .setPeerAddress(packet.getPeerAddress())
+                        .setPayload(middleData.getBytes())
+                        .create();
+
+                sendWindow.put(sequenceNumber,false);
+
+                sendPacket = new SendPacket(middlePacket,this);
+                sendPacketThread = new Thread(sendPacket);
+                sendPacketThread.start();
+                sequenceNumber++;
+            }
+
+            Packet lastPacket = new Packet.Builder()
+                    .setType(7)
+                    .setSequenceNumber(sequenceNumber)
+                    .setPortNumber(packet.getPeerPort())
+                    .setPeerAddress(packet.getPeerAddress())
+                    .setPayload(response.getBytes())
+                    .create();
+
+            sendWindow.put(sequenceNumber,false);
+
+            sendPacket = new SendPacket(lastPacket,this);
+            sendPacketThread = new Thread(sendPacket);
+            sendPacketThread.start();
+            System.out.println("last sent!!");
         }
     }
 
@@ -95,6 +203,14 @@ public class NewConnection implements Runnable {
         sequenceNumber ++;
     }
 
+    public boolean responseAllSent(){
+        for (long key: sendWindow.keySet()){
+            if(!sendWindow.get(key)){
+                return false;
+            }
+        }
+        return true;
+    }
 
     public long getSequenceNumber(){
         return sequenceNumber;
@@ -113,32 +229,16 @@ public class NewConnection implements Runnable {
         return packet;
     }
 
-    public void increaseSequenceNumber(){
-        sequenceNumber ++;
-    }
-
-    public boolean getConnected(){
-        return connected;
-    }
-
     public void setConnected(boolean connected){
         this.connected = connected;
     }
 
-    public void setRcvData(String data){
-        this.rcvData = data;
+    public void setAllSent(boolean allSent){
+        this.allSent = allSent;
     }
 
-    public String getRcvData(){
-        return rcvData;
-    }
-
-    public void setFin(boolean fin){
-        this.fin = fin;
-    }
-
-    public boolean getFin(){
-        return fin;
+    public boolean getAllSent(){
+        return allSent;
     }
 
     public long getStartNumber(){
@@ -149,8 +249,8 @@ public class NewConnection implements Runnable {
         return endNumber;
     }
 
-    public TreeMap<Long,String> getWindow(){
-        return window;
+    public TreeMap<Long,String> getRcvWindow(){
+        return rcvWindow;
     }
 
     public void setStartNumber(long startNumber){
@@ -159,6 +259,10 @@ public class NewConnection implements Runnable {
 
     public void setEndNumber(long endNumber){
         this.endNumber = endNumber;
+    }
+
+    public HashMap<Long,Boolean> getSendWindow(){
+        return sendWindow;
     }
 
 }
